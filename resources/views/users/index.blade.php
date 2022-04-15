5a70bd1322f636e1825f7ceec49378c7d6b8b04d
Thales Pereira <thales.pereira@jdrel.com.br>
2022-04-15 18:35:13 -0300

    feature/paginação

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -71,6 +71,12 @@
         </tbody>
     </table>
 
+    <div class="py-4">
+        {{ $users->appends([
+                'search' => request()->get('search', ''),
+            ])->links() }}
+    </div>
+
     {{-- <div class="py-4">
         {{ $users->appends([
                 'search' => request()->get('search', ''),
