3c4438d7c8a1721dae07bbed26e286dc434ce930
Thales Pereira <thales.pereira@jdrel.com.br>
2022-04-16 19:57:53 -0300

    feature/autenticação

--- routes/web.php
+++ routes/web.php
@@ -17,22 +17,26 @@ use App\Http\Controllers\Admin\CommentController;
 |
 */
 
-Route::delete('/users/{idUser}/comments/{idComment}', [CommentController::class, 'destroy'])->name('comments.destroy');
-Route::post('/users/{id}/comments', [CommentController::class, 'store'])->name('comments.store');
-Route::get('/users/{id}/comments/create', [CommentController::class, 'create'])->name('comments.create');
-Route::get('/users/{id}/comments', [CommentController::class, 'index'])->name('comments.index');
-Route::get('/users/{idUser}/comments/{idComment}', [CommentController::class, 'show'])->name('comments.show');
-Route::get('/users/{idUser}/comments/{idComment}/edit', [CommentController::class, 'edit'])->name('comments.edit');
-Route::put('/users/{idUser}/comments/{idComment}', [CommentController::class, 'update'])->name('comments.update');
+Route::middleware(['auth'])->group(function () {
+    Route::delete('/users/{idUser}/comments/{idComment}', [CommentController::class, 'destroy'])->name('comments.destroy');
+    Route::post('/users/{id}/comments', [CommentController::class, 'store'])->name('comments.store');
+    Route::get('/users/{id}/comments/create', [CommentController::class, 'create'])->name('comments.create');
+    Route::get('/users/{id}/comments', [CommentController::class, 'index'])->name('comments.index');
+    Route::get('/users/{idUser}/comments/{idComment}', [CommentController::class, 'show'])->name('comments.show');
+    Route::get('/users/{idUser}/comments/{idComment}/edit', [CommentController::class, 'edit'])->name('comments.edit');
+    Route::put('/users/{idUser}/comments/{idComment}', [CommentController::class, 'update'])->name('comments.update');
 
-Route::delete('/users/destroy/{id}', [UserController::class, 'destroy'])->name('users.destroy');
-Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
-Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
+    Route::delete('/users/destroy/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
+    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+require __DIR__ . '/auth.php';
